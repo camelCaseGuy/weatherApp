293791d2e4baae501f8f0baee21a1525436e2665
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-18 14:42:32 -0500

    added json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import myJson from './response.json' assert {type: 'json'};
 const output = document.getElementById('response');
 fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Sachse%2C%20TX?unitGroup=us&key=EXPP9ZFW7559NA2WLHV699P3P&contentType=json", {
   "method": "GET",
@@ -7,9 +8,14 @@ fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/service
 .then(response => {
   // response.days.forEach(day => {
     
-  // });
-  output.innerHTML = JSON.stringify(response.days);
+    // });
   console.log(response);
+  if (response.days) {
+    output.innerHTML = JSON.stringify(response.days);
+  } else {
+    output.innerHTML = JSON.stringify(myJson);
+  }
+
 })
 .catch(err => {
   console.error(err);
